d74d7cf55badf70c94c2cc2d7b5af520811d8aef
朝洋 s <15657868169m@sina.cn>
2020-06-20 18:17:08 +0800

    add swagger.

--- Blog.Core/Blog.Core/Startup.cs
+++ Blog.Core/Blog.Core/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Blog.Core
 {
@@ -21,10 +22,29 @@ namespace Blog.Core
 
         public IConfiguration Configuration { get; }
 
+        public string ApiName { get; set; } = "Blog.Core";
+
+        //var basePath = Microsoft.DotNet.PlatformAbstractions.ApplicationEnvironment.ApplicationBasePath;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V1", new OpenApiInfo
+                {
+                    // {ApiName} 定义成全局变量，方便修改
+                    Version = "V1",
+                    Title = $"{ApiName} 接口文档——Netcore 3.0",
+                    Description = $"{ApiName} HTTP API V1",
+                    Contact = new OpenApiContact { Name = ApiName, Email = "Blog.Core@xxx.com", Url = new Uri("https://www.jianshu.com/u/94102b59cc2a") },
+                    License = new OpenApiLicense { Name = ApiName, Url = new Uri("https://www.jianshu.com/u/94102b59cc2a") }
+                });
+                c.OrderActionsBy(o => o.RelativePath);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +54,17 @@ namespace Blog.Core
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/V1/swagger.json", $"{ApiName} V1");
+
+                //路径配置，设置为空，表示直接在根域名（localhost:8001）
+                //访问该文件,注意localhost:8001/swagger是访问不到的，
+                //去launchSettings.json把launchUrl去掉，如果你想换一个路径，直接写名字即可，比如直接写c.RoutePrefix = "doc";
+                c.RoutePrefix = "";
+            });
 
             app.UseRouting();
 
